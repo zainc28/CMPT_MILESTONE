887cf875fb2f8d659f92282bca2a483ed6312634
Zain Choudhry <zain.choudhry2004@gmail.com>
2025-03-28 17:42:16 -0700

    update
    still need to implement menu option 5, nearing final stages

--- CMPT_Milestone1_ZC_AK.py
+++ CMPT_Milestone1_ZC_AK.py
@@ -1,52 +1,16 @@
 # Zain Choudhry, Aaryan Khanal
 # Programming Project - Milestone#1
 
-
-'''
-Planning
-Milestone #1 Due Date: March 30th, 11:59 PM
-This milestone focuses on:
-1. Loading two of the ETS files (trips.txt and shapes.txt) into appropriate data structures
-2. Developing a text interface to access and search these data structures
-3. Serializing these data structures using the pickle module for quick loading
-
-Objectives of the project:
-        1. Read three of the ETS files (routes.txt, trips.txt and shapes.txt), and
-store the information in appropriate data structures.
-        2. Develop a text interface to access and search the data.
-        3. Serialize the data structures using the pickle module for quick loading
-
-
-three data files:
-– routes.txt
-• route_id (col 0), e.g. '008'
-• route_name (col 3), e.g. "Abbottsfield - Downtown - University".
-
-– trips.txt
-• route_id (col 0)
-• shape_id (col 6), multiple for each route_id,
-– e.g. 008-210-South, 008-211-North
-
-– shapes.txt
-• shape_id (col 0)
-• latitude (col 1), longitude (col 2) locations. Many for each shape_id
-• Other data columns can be ignored. 
-
-
-During implementation, you must deploy appropriate strategies learned in lecture that prohibit your program from
-crashing due to:
-• Invalid user input
-• Performing an action before a file is loaded
-• Attempting to open a file for reading or writing
-• Closing the interactive map prematurely
-Necessary safeguards should be in place.
-'''
+import pickle
 
 def main():
     route_data = {}
+    route_names = {}
     shapes_data = {}
 
-    menu_input = input('''Edmonton Transit Systems
+    menu_loop = True
+    while menu_loop:
+        menu_input = input('''\nEdmonton Transit Systems
 --------------------------------
 (1) Load route data 
 (2) Load shapes data
@@ -58,41 +22,41 @@ def main():
 (8) Load routes and shapes from a pickle
 (9) Reserved for future use
 (0) Quit\n''')
-
-    if menu_input == '1':
-        route_data = load_route_data()
-    elif menu_input == '2':
-        pass
-    elif menu_input == '3':
-        print('Option 3 reserved for Milestone 2')
-    elif menu_input == '4':
-        pass
-    elif menu_input == '5':
-        pass
-    elif menu_input == '6':
-        print('Option 6 reserved for Milestone 2')
-    elif menu_input == '7':
-        pass
-    elif menu_input == '8':
-        pass
-    elif menu_input == '9':
-        print('Option 9 reserved for Milestone 2')
-    elif menu_input == '0':
-        quit()
+        if menu_input == '1':
+            route_data, route_names = load_route_data()
+        elif menu_input == '2':
+            shapes_data = load_shapes_data()
+        elif menu_input == '3':
+            print('Option 3 reserved for Milestone 2')
+        elif menu_input == '4':
+            shape_for_route(route_data, route_names)
+        elif menu_input == '5':
+            pass
+        elif menu_input == '6':
+            print('Option 6 reserved for Milestone 2')
+        elif menu_input == '7':
+            save_pickle(route_data, route_names, shapes_data)
+        elif menu_input == '8':
+            route_data, route_names, shapes_data = load_pickle()
+        elif menu_input == '9':
+            print('Option 9 reserved for Milestone 2')
+        elif menu_input == '0':
+            menu_loop = False
+            print('Goodbye!')
 
 
 def load_route_data() -> dict:
     """
     purpose: Create a data structure retrieving and storing route id, 
-            route name and shape ids(without duplicates) all together 
-    parameters: 
-    returns: 
-
-1. recieve the filename from the user with error handling 
-2. read the routes text file and retrieve route name and create a dictionary 
-3. read the trips file and then return route id and shape id put in a dictionary 
-4. put shape id into a set to remove duplicates so we only end up with unique sets 
+             route name and shape ids(without duplicates) all together
+                1. recieve the filename from the user with error handling 
+                2. read the routes text file and retrieve route name and create a dictionary 
+                3. read the trips file and then return route id and shape id put in a dictionary 
+                4. put shape id into a set to remove duplicates so we only end up with unique sets 
+    parameters: none
+    returns: dictionary of route data and route names
     """
+
     while True:
         filename_input = input("Enter trips filename (Press enter for default 'data/trips.txt'): ").strip()
         trips_file = filename_input if filename_input else 'data/trips.txt'
@@ -106,8 +70,8 @@ def load_route_data() -> dict:
             print(f"IOError: Couldn't open non_existent_file")
 
     routes = {} 
+    route_names = {}
 
-    #ASK THE PROF IS THIS PART IS SUPPOSED TO BE HARDCODED????
     try:
         with open('data/routes.txt', 'r') as file:
             next(file)
@@ -116,6 +80,7 @@ def load_route_data() -> dict:
                 if len(data) > 3:  
                     route_id = data[0].strip()
                     route_name = data[3].strip()
+                    route_names[route_id] = route_name
                     routes[route_id] = [route_name, set()]
     except FileNotFoundError:
         print('Routes file not found')
@@ -135,24 +100,112 @@ def load_route_data() -> dict:
         print('File not found')
         return {}
     print(f"Data from {trips_file} loaded")
-    return routes
+    return routes, route_names
         
 
 def load_shapes_data():
-    pass
+    """
+    purpose: Create a data structure retrieving and storing shape id, lat and lon all together
+    parameters: none
+    returns: dictionary of shape data
+    """
+    while True:
+        filename_input = input("Enter trips filename (Press enter for default 'data/shapes.txt'): ").strip()
+        trips_file = filename_input if filename_input else 'data/shapes.txt'
+        
+        try:
+            open(trips_file).close()  
+            break
+        except FileNotFoundError:
+            print(f"IOError: Couldn't open non_existent_file")
+        except Exception as e:
+            print(f"IOError: Couldn't open non_existent_file")
+    shapes = {}
+    try:
+        with open(trips_file, 'r') as file:       
+            next(file)
+            for line in file:
+                parts = line.strip().split(',')
+            
+                shape_id = parts[0]
+                lat = float(parts[1])
+                lon = float(parts[2])
+            
+                if shape_id not in shapes:
+                    shapes[shape_id] = []
+                
+                shapes[shape_id].append((lat, lon))
+    except FileNotFoundError:
+        print('File not found error')
+        return {}
+    print(f"Data from {trips_file} loaded")
+    return shapes
+   
 
-def shape_for_route():
-    #if file isnt loaded from option 1 then print
-    route_data = load_route_data()
+def shape_for_route(routes, route_names):
+    """
+    purpose: allows user to enter a route id and then prints the shape ids for that route
+    parameters: routes dictionarty, route_names dictionary
+    returns: none
+    """
+    if not routes or not route_names:
+        print("Route data hasn't been loaded yet")
+        return
+    
+    route_id = input("Enter route: ").strip()
 
-    if not filename_input:
-        print("Route data hasn 't been loaded yet")
+    if route_id in routes:
+        shape_ids = routes[route_id][1]
+        print(f'''Shapes ids for route [{route_names[route_id]}] 
+    {''' 
+    '''.join(shape_ids)}''')
     else:
-        route_id = input("Enter route: ").strip()
-        if route_id in route_data:
-            print(f'''Shapes ids for route [{route_name} 
-    {route_data[route_id][1]}]''')
-        else:
-            print(f"** NOT FOUND **")  
+        print(f"** NOT FOUND **")  
+
+def save_pickle(routes, route_names, shapes):
+    """
+    purpose: the user is prompted for filename which is then opened for writing. 
+            If the user presses enter without entering a filename, the filename 
+            defaults to etsdata.p. The Data structures containing the route and 
+            shapes data are written to the binary file. The pickle module is used 
+            to write these data structures to the file. This operation should not 
+            fail if the data structures have not been loaded yet. 
+    parameters: routes dictionary, route_names dictionary, shapes dictionary
+    returns: none
+    """
+    
+    if not routes or not route_names or not shapes:
+        print("Data hasn't been loaded yet")
+        return
+    
+    pickle_file_input = input("Enter a filename (Press enter for default 'data/etsdata.p'): ").strip()
+    pickle_file = pickle_file_input if pickle_file_input else 'data/etsdata.p'
+    try:
+        with open(pickle_file, 'wb') as file:
+            pickle.dump((routes, route_names, shapes), file)
+        print(f"Data structures successfully written to data {pickle_file}")
+    except FileNotFoundError:
+        print(f"IOError: Couldn't open non_existent_file")
+
+def load_pickle():
+    """
+    purpose: the user is prompted for a filename which is then opened for reading. 
+            If the user presses enter without entering a filename, the filename 
+            defaults to etsdata.p. The Data structures containing the route and 
+            shapes data are read from the binary file. The pickle module is used
+            to read these data structures from the file. 
+    parameters: none
+    returns: routes dictionary, route_names dictionary, shapes dictionary
+    """
+    pickle_file_input = input("Enter a filename (Press enter for default 'data/etsdata.p'): ").strip()
+    pickle_file = pickle_file_input if pickle_file_input else 'data/etsdata.p'
+    try:
+        with open(pickle_file, 'rb') as file:
+            routes, route_names, shapes = pickle.load(file)
+        print(f"Routes and shapes Data structures successfully loaded from {pickle_file}")
+        return routes, route_names, shapes
+    except FileNotFoundError:
+        print(f"IOError: Couldn't open non_existent_file")
+        return {}, {}, {}
 
 main()
